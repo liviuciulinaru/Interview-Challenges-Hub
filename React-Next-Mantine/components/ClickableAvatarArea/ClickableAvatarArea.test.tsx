27e2b2e5557657f9bfc873c934417123257cf95a
Liviu Ciulinaru <liviu.ciulinaru@gmail.com>
2024-02-08 14:25:13 +0700

    remove comments

--- React-Next-Mantine/components/ClickableAvatarArea/ClickableAvatarArea.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@/test-utils';
-import { Error } from './ClickableAvatarArea';
-
-describe('Error component', () => {
-  it('has correct Next.js theming section link', () => {
-    render(<Error />);
-    expect(screen.getByText('Error')).toHaveAttribute('href', 'https://mantine.dev/guides/next/');
-  });
-});
